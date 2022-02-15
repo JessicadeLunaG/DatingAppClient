9e7f555f40f1da0f9d0a1f487779aa580ad77da2
Jessica de Luna <mr.cosmos.math@gmail.com>
2022-02-14 19:22:44 -0600

    pipe async implementation

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -8,19 +10,18 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  loggedIn: boolean;
+  currentUser$: Observable<User>;
 
   constructor(private accountService:AccountService) { }
 
   ngOnInit(): void {
-    this.getCurrentUser();
+    this.currentUser$ = this.accountService.currentUser$;
   }
 
   login(){
     this.accountService.login(this.model).subscribe(
       response => {
         console.log(response);
-        this.loggedIn = true;
       }, error => {
         console.log(error)
       }
@@ -29,15 +30,6 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout(); //llamamos al servicio para que haga la llamada
-    this.loggedIn =false;
   }
 
-  //establece el estado del usuario logeado
-  getCurrentUser(){
-    this.accountService.currentUser$.subscribe(user=>{
-      this.loggedIn = !!user;
-    }), error =>{
-      console.log(error);
-    }
-  }
 }
