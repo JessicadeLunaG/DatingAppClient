7039d7caea1be0316700434a3f0ff44ec8e2d190
Jessica de Luna <mr.cosmos.math@gmail.com>
2022-03-21 14:34:47 -0600

    [FIX] Validation Error

--- src/app/errors/test-errors/test-errors.component.ts
+++ src/app/errors/test-errors/test-errors.component.ts
@@ -23,7 +23,7 @@ export class TestErrorsComponent implements OnInit {
     })
   }
   get400ValidationError(){
-    this.http.get(this.baseUrl + 'buggy/not-found').subscribe(response=>{
+    this.http.post(this.baseUrl + 'account/register', {}).subscribe(response=>{
       console.log(response);
     }, error =>{
       console.log(error);
