b83f26a26eeda710ae4022eda7e7d937155e6638
Jessica de Luna <mr.cosmos.math@gmail.com>
2022-04-03 10:55:40 -0500

    [ADD] _models and token

---
+++ src/app/_forms/text-imput/text-imput.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-imput',
+  templateUrl: './text-imput.component.html',
+  styleUrls: ['./text-imput.component.css']
+})
+export class TextImputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
