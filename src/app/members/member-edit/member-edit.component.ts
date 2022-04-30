c105ecef71d99df95c11004856f51fba4a29edab
Jessica de Luna <mr.cosmos.math@gmail.com>
2022-04-30 17:52:04 -0500

    [ADD] Spinner

--- src/app/members/member-edit/member-edit.component.ts
+++ src/app/members/member-edit/member-edit.component.ts
@@ -38,9 +38,10 @@ export class MemberEditComponent implements OnInit {
   }
 
   updateMember(){
-    console.log(this.member);
-    this.toastr.success("Your profile has been updateded!");
-    this.editForm.reset(this.member);
+    this.memberService.updateMember(this.member).subscribe(()=>{
+      this.toastr.success("Your profile has been updateded!");
+      this.editForm.reset(this.member);
+    })
   }
 
 }
