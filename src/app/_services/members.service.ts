291d41ad220dfecb7257e45aeef741e3d16d856c
Jessica de Luna <mr.cosmos.math@gmail.com>
2022-05-04 05:56:53 -0500

    [FIX] memberCache

--- src/app/_services/members.service.ts
+++ src/app/_services/members.service.ts
@@ -60,9 +60,11 @@ export class MembersService {
   }
 
   getMember(username:string): Observable<Member>{
-    const member = [...this.memberCache.values()];
-    //this.members.find(x => x.userName === username);
-    //if(member !== undefined) return of(member); 
+    const member = [...this.memberCache.values()]
+      .reduce((arr, elem) => arr.concat(elem.result), [])
+      .find((member: Member) => member.userName === username);
+    
+    if(member) return of(member); 
     return this.http.get<Member>(this.baseUrl + "users/" + username);
   }
 
