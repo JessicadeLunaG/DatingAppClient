0565b7cac0869de751a7bb4b934ebc6164971953
Jessica de Luna <mr.cosmos.math@gmail.com>
2022-06-03 11:42:37 -0500

    test-errors component

--- src/app/errors/test-errors/test-errors.component.ts
+++ src/app/errors/test-errors/test-errors.component.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { response } from 'express';
 
 @Component({
   selector: 'app-test-errors',
@@ -8,46 +7,46 @@ import { response } from 'express';
   styleUrls: ['./test-errors.component.css']
 })
 export class TestErrorsComponent implements OnInit {
-  baseUrl= 'https://localhost:5001/api/';
+  baseUrl = 'https://localhost:5001/api/';
   validationErrors: string[] = [];
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
-  get400Error(){
+  get400Error() {
     this.http.get(this.baseUrl + 'buggy/bad-request').subscribe({
-      next: (response)=> console.log(response),
+      next: (response) => console.log(response),
       error: (error) => console.log(error)
-    });
+    })
   }
-  get400ValidationError(){
+  get400ValidationError() {
     this.http.post(this.baseUrl + 'account/register', {}).subscribe({
-      next: (response)=> console.log(response),
+      next: (response) => console.log(response),
       error: (error) => {
         console.log(error);
         this.validationErrors = error;
       }
-    });
+    })
   }
-  get401Error(){
+  get401Error() {
     this.http.get(this.baseUrl + 'buggy/auth').subscribe({
-      next: (response)=> console.log(response),
+      next: (response) => console.log(response),
       error: (error) => console.log(error)
-    });
+    })
   }
-  get404Error(){
+  get404Error() {
     this.http.get(this.baseUrl + 'buggy/not-found').subscribe({
-      next: (response)=> console.log(response),
+      next: (response) => console.log(response),
       error: (error) => console.log(error)
-    });
+    })
   }
-  get500Error(){
+  get500Error() {
     this.http.get(this.baseUrl + 'buggy/server-error').subscribe({
-      next: (response)=> console.log(response),
+      next: (response) => console.log(response),
       error: (error) => console.log(error)
-    });
+    })
   }
 
 }
